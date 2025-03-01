d727d25895a8fc4c7b890186b418c779a96ab695
Justin Takamiya <justintakamiya@justins-mbp-3.lan>
2025-03-01 05:56:25 -0500

    Created scoreboard template

---
+++ index.js
@@ -0,0 +1,25 @@
+LoadEverything().then(() => {
+
+  Start = async () => {
+  };
+
+  Update = async (event) => {
+    let data = event.data;
+
+    for (const [t, team] of [
+      data.score[window.scoreboardNumber].team["1"],
+      data.score[window.scoreboardNumber].team["2"],
+    ].entries()) {
+      for (const [p, player] of [team.player["1"]].entries()) {
+        if (player) {
+          
+        }
+        if (team.color) {
+          document
+            .querySelector(":root")
+            .style.setProperty(`--p${t + 1}-score-bg-color`, team.color);
+        }
+      }
+    }
+  };
+});
