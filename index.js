e55f92267e8b654a977efa0ea4c5471813b29d64
Justin Takamiya <105453114+JinchanJ@users.noreply.github.com>
2025-08-21 22:47:40 -0400

    Added insert code here comment in JS file

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ LoadEverything().then(() => {
     ].entries()) {
       for (const [p, player] of [team.player["1"]].entries()) {
         if (player) {
-          
+          // Insert code here
         }
         if (team.color) {
           document
