ae61be2667d1374ca7d683b61e1d99f0ad8fc54b
Justin Takamiya <105453114+JinchanJ@users.noreply.github.com>
2025-12-28 15:05:38 -0500

    Simplified logic of index.js

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 LoadEverything().then(() => {
-
+  
   Start = async () => {
+    
   };
 
   Update = async (event) => {
-    let data = event.data;
-
-    for (const [t, team] of [
-      data.score[window.scoreboardNumber].team["1"],
-      data.score[window.scoreboardNumber].team["2"],
-    ].entries()) {
-      for (const [p, player] of [team.player["1"]].entries()) {
-        if (player) {
-          // Insert code here
-        }
-        if (team.color) {
-          document
-            .querySelector(":root")
-            .style.setProperty(`--p${t + 1}-score-bg-color`, team.color);
-        }
+    const team1 = event.data.score[window.scoreboardNumber].team["1"];
+    const team2 = event.data.score[window.scoreboardNumber].team["2"];
+    const teams = [team1, team2];
+    
+    for (const [t, team] of teams.entries()) {
+      
+      const player = team.player["1"];
+      
+      if (player) {
+        // TODO: Insert code here
+        
+        // For team/player info:
+        // SetInnerHtml($(`.p${t + 1}`), );
+        
+        // For tournament info:
+        // SetInnerHtml($(""), );
       }
+      
+      if (team.color) {
+        document.querySelector(":root").style.setProperty(`--p${t + 1}-score-bg-color`, team.color);
+      }
+      
     }
   };
 });
